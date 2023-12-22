382dd1f9e72ce9302fb933fdfaea75119eb51d30
Mehrabios <mohamadhosin.anbarloo@gmail.com>
2023-12-22 15:09:29 +0330

    Styles and scripts completed

---
+++ JS/stylewave.js
@@ -0,0 +1,15 @@
+/* All rights to StyleWave are reserved by Mohammad Hossein Anabarlou (Mehrab Anbarlou).
+
+/* basic styles code */
+
+const allElements = document.querySelectorAll("*");
+allElements.forEach(function (element){
+    element.style.transition = '0.5s';
+    element.style.boxSizing = 'border-box';
+    element.style.fontFamily = 'sans-serif';
+})
+
+/* basic styles code end */
+
+/* Copyright
+(c) 2023 Mohammad Hossein Anbarlou (Mehrab Anbarlou) */
