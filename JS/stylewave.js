8fdec799418ccf929465bbb2de07df129b2b0496
Mehrabios <mohamadhosin.anbarloo@gmail.com>
2023-12-23 22:29:29 +0330

    input styles and night/light mode added

--- JS/stylewave.js
+++ JS/stylewave.js
@@ -1,15 +1,50 @@
-/* All rights to StyleWave are reserved by Mohammad Hossein Anabarlou (Mehrab Anbarlou).
+/* All rights to StyleWave are reserved by Mohammad Hossein Anabarlou (Mehrab Anbarlou). */
 
-/* basic styles code */
+/* Variable introduction code */
 
 const allElements = document.querySelectorAll("*");
+const htmlElement = document.querySelector("html");
+const bodyElement = document.querySelector("body");
+
+/* Variable introduction code end */
+
+/* Basic styles */
+
 allElements.forEach(function (element){
     element.style.transition = '0.5s';
     element.style.boxSizing = 'border-box';
     element.style.fontFamily = 'sans-serif';
-})
+});
 
-/* basic styles code end */
+/* Basic styles end */
 
-/* Copyright
-(c) 2023 Mohammad Hossein Anbarlou (Mehrab Anbarlou) */
+/* Theme Checker code */
+
+function getTheme() {
+    if (htmlElement.getAttribute('stylewave-theme') === 'dark') {
+        return 'dark';
+    }
+    else if (htmlElement.getAttribute('stylewave-theme') === 'light') {
+        return 'light';
+    }
+}
+
+function themeChanger(theme) {
+    if (theme === 'dark') {
+        allElements.forEach(function (element) {
+            element.style.color = '#e4e4e4';
+        });
+        bodyElement.style.backgroundColor = '#222222';
+    } else if (theme === 'light') {
+        allElements.forEach(function (element) {
+            element.style.color = '#222222';
+        });
+        bodyElement.style.backgroundColor = 'white';
+    }
+}
+
+themeChanger(getTheme());
+
+/* Theme Checker code end */
+
+/* Copyright (c) 2023 Mohammad Hossein Anbarlou (Mehrab Anbarlou) */
